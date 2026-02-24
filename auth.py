943e379f38e9dda44e8f86a60d32af90371fafe2
Nishant Jain <nishant.jain@hitechdigital.com>
2026-02-24 23:15:42 +0530

    fix : asyncsession included

--- auth.py
+++ auth.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 import jwt
 from fastapi import HTTPException, Depends, status
 from sqlalchemy.orm import Session
@@ -6,7 +8,7 @@ from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
 
 from models.user import User
-from core.database import get_db
+from core.database import async_get_db
 from core.config import settings
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
@@ -16,6 +18,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
@@ -30,8 +33,8 @@ def create_access_token(data: dict):
     return jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
 
-def get_current_user(
-    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+async def get_current_user(
+    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(async_get_db)
 ):
     try:
         payload = jwt.decode(
@@ -39,25 +42,30 @@ def get_current_user(
         )
         email: str = payload.get("sub")
         if email is None:
-            raise HTTPException(status_code=401, detail="Invalid token: missing subject")
-            
+            raise HTTPException(
+                status_code=401, detail="Invalid token: missing subject"
+            )
+
     except jwt.ExpiredSignatureError:
         raise HTTPException(status_code=401, detail="Token has expired")
     except jwt.PyJWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
 
-    user = db.query(User).filter(User.email == email).first()
+    user = await db.execute(select(User).where(User.email == email))
+    user = user.scalars().first()
     if not user:
         raise HTTPException(status_code=401, detail="nhi mila")
 
     return user
 
+
 def require_roles(*allowed_roles: str):
     def role_checker(current_user: User = Depends(get_current_user)):
         if current_user.role not in allowed_roles:
             raise HTTPException(
-                status_code=status.HTTP_403_FORBIDDEN, 
-                detail=f"Abey! {current_user.role} allowed nahi h. Only {allowed_roles} can enter."
+                status_code=status.HTTP_403_FORBIDDEN,
+                detail=f"Abey! {current_user.role} allowed nahi h. Only {allowed_roles} can enter.",
             )
         return current_user
+
     return role_checker
